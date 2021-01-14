03790c88886c3388b1cf9df1fd91a31864541a1b
Francis <francislabounty@yahoo.com>
2021-01-13 17:34:35 -0700

    fixed helper_fuctions.py

--- helper_functions.py
+++ helper_functions.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
 
-class DfHelper(self, df):
+class DfHelper:
     """
     A class that contains various pandas dataframe helper functions
     """
-    self.df = df
+    def __init__(self, df):
+        self.df = df
 
     def null_count(self):
         return self.df.isna().sum()
@@ -13,3 +14,6 @@ class DfHelper(self, df):
     def randomize(self, seed):
         self.df = self.df.apply(np.random(seed=seed).permutation, axis=0)
         return self.df.apply(np.random(seed=seed).permutation, axis=1)
+
+if __name__ == "__main__":
+    print("File loaded and running successfully")
